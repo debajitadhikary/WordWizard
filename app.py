cf175c828eb6323abe2b29afb73229b9d09effe0
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2025-04-16 12:50:48 +0530

    revamp

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import math
 
 app = Flask(__name__)
 
@@ -9,20 +10,44 @@ def index():
 
 @app.route('/search')
 def search():
-    query = request.args.get('query')
+    query = request.args.get('query', '').strip()
+    page = int(request.args.get('page', 1))
+    per_page = 10  # You can change this value if you want more/less results per page
+
     if query:
         conn = sqlite3.connect('_dictionary.db')
         cur = conn.cursor()
-        cur.execute("SELECT * FROM words WHERE word=?", (query,))
+
+        # Count total matching results
+        cur.execute("SELECT COUNT(*) FROM words WHERE word LIKE ?", ('%' + query + '%',))
+        total_results = cur.fetchone()[0]
+
+        total_pages = math.ceil(total_results / per_page)
+        offset = (page - 1) * per_page
+
+        # Fetch paginated results
+        cur.execute(
+            "SELECT * FROM words WHERE word LIKE ? LIMIT ? OFFSET ?",
+            ('%' + query + '%', per_page, offset)
+        )
         results = cur.fetchall()
         conn.close()
+
         if results:
-            return render_template('search.html', results=results)
+            return render_template(
+                'search.html',
+                results=results,
+                query=query,
+                page=page,
+                total_pages=total_pages
+            )
         else:
-            return render_template('search.html', no_results=True)
+            return render_template(
+                'search.html',
+                query=query,
+                page=page,
+                total_pages=total_pages,
+                no_results=True
+            )
+
     return render_template('search.html')
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
-    # app.run(debug=True)
-
